c01a5fe5672cb3f1194d1fe66a91c0e12e993380
bobblakley <43755082+bobblakley@users.noreply.github.com>
2021-03-21 12:27:57 -0500

    Initial Working Python Version

---
+++ main.py
@@ -0,0 +1,41 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from mainwindow import Ui_MainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.CustomInitialization()
+
+    def CustomInitialization(self):
+        QObject.connect(self.ui.findButton, SIGNAL("clicked()"), self.find_clicked)
+        self.loadTextFile()
+
+    def find_clicked(self):
+        self.searchString = self.ui.lineEdit.text()
+        self.ui.textEdit.find(self.searchString, QTextDocument.FindWholeWords)
+
+    def loadTextFile(self):
+        self.inputFile = QFile("input.txt")
+        if self.inputFile.open(QIODevice.ReadOnly | QIODevice.Text):
+            self.textStrmData = QTextStream(self.inputFile)
+            self.text = self.textStrmData.readAll()
+            self.inputFile.close()
+        else:
+            self.text = "File Open failed"
+        self.ui.textEdit.setPlainText(self.text)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec_())
+
---
+++ mainwindow.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(70, 50, 55, 16))
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(150, 50, 113, 22))
+        self.findButton = QPushButton(self.centralwidget)
+        self.findButton.setObjectName(u"findButton")
+        self.findButton.setGeometry(QRect(280, 50, 93, 28))
+        self.textEdit = QTextEdit(self.centralwidget)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(73, 96, 301, 221))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 26))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Keyword", None))
+        self.findButton.setText(QCoreApplication.translate("MainWindow", u"Find", None))
+    # retranslateUi
+
